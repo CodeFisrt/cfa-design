d4c2374817f332b0c95736c531a67f6bf0cfe088
kadamsarika <sarikakadam662@gmail.com>
2022-08-25 16:20:09 +0530

    javascript day-2 task

---
+++ day-2.js
@@ -0,0 +1,20 @@
+//object
+var data = ['data','Engineer',8,true];
+console.log("Data type is", typeof(data));
+//undefined
+console.log("Data type is", typeof(codeFirst))
+//number
+var count = 3;
+console.log("Data type is", typeof(count))
+//string
+var sstring = 'codefirst';
+console.log("Data type is", typeof(sstring))
+//Boolean
+var expression = true;
+console.log("Data type is", typeof(expression))
+//boolean
+var expression = false;
+console.log("Data type is", typeof(expression))
+//null
+var nullvalu = null;
+console.log("Datatype is", typeof(nullvalu))
